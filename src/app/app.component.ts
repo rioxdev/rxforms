30ed9f646c96e6e81ead4afccce3857dea2b5bce
henrio randriasoa <rioxdev@gmail.com>
2023-03-26 11:52:35 +0200

    stage 2

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,38 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { FormControl, FormGroup, NgForm, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'rxforms';
+
+  nameField: FormControl = new FormControl("rx");
+
+  productForm: FormGroup = new FormGroup({
+    name: new FormControl("rx", Validators.required)
+  });
+
+  ngOnInit() {
+    this.nameField.valueChanges.subscribe(x => {
+
+    });
+
+    this.productForm.statusChanges.subscribe(newStatus => {
+      console.log(newStatus);
+    });
+  }
+
+  resetForm() {
+
+    this.nameField.reset();
+  }
+
+  submitForm() {
+    console.log("form submitted");
+    console.log("name : " + this.productForm.controls["name"].value);
+  }
+
 }
